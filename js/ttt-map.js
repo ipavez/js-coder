dbea86d01c825d2ef8951f8e10d554e8bdeee164
ipavez <ipavezurbina@gmail.com>
2024-06-07 16:55:48 -0400

    localStorage

--- js/ttt-map.js
+++ js/ttt-map.js
@@ -3,6 +3,14 @@ const loginMap = new Map([
     ['coder', 'house'],
     ['snk', 'mi'],
 ]);
+if(localStorage.newUsers != undefined){
+    const newUsers = JSON.parse(localStorage.newUsers);
+    for (const [newUser,pass] of newUsers){
+        if(!loginMap.has(newUser)){
+        loginMap.set(newUser,pass);
+        }
+    }
+}
 
 class Report{
     constructor(user,date, txt, prioridad = 'baja', ){
@@ -16,20 +24,17 @@ class Report{
         Report.allInstances.push(this);     
     }
     altaPrioridad(){
-        this.prioridad = 'alta'
+        this.prioridad = 'alta';
         
     }
-    editReport(){
-        const txt = this.txt;
-        const new_txt = prompt('Editar reporte');
-        const edit_txt = `Edited report: ${new_txt}`+ '\n' + `Original Report:${this.user}-${this.date}: ${txt}`;
-        const newDate = new Date();
-        return new Report(this.user,newDate,edit_txt);    
-    }
 }
-
 Report.allInstances=[];
-
+if(localStorage.newReports != undefined){
+    const newReportList = JSON.parse(localStorage.newReports);
+    for(report of newReportList){
+        Report.allInstances.push(report);
+    }  
+}
 
 function loginUser(usuario){
     let block = true;
@@ -42,7 +47,7 @@ function loginUser(usuario){
             else if (password == loginMap.get(usuario)){
                 block = false;
                 target = document.getElementById('saludo');
-                target.innerHTML = `Hola ${usuario}`
+                target.innerHTML = `Hola ${usuario}`;
                 target.style.color = 'green';
                 return usuario;
             }
@@ -53,7 +58,7 @@ function loginUser(usuario){
         else if(usuario == '+newUser'){
             let nuevoUsuario = prompt('Ingrese nuevo usuario:');
             if(loginMap.has(nuevoUsuario)){
-                alert('Usuario ya existe')
+                alert('Usuario ya existe');
             }
             else if(nuevoUsuario == '+newUser') {
                 alert('Nombre usuario invalido');
@@ -62,6 +67,7 @@ function loginUser(usuario){
             else{
                 const newPass = prompt('Establesca una contraseña:');
                 loginMap.set(nuevoUsuario,newPass);
+                localStorage.newUsers = JSON.stringify(Array.from(loginMap.entries()));
                 alert(`Usuario ${nuevoUsuario} creado exitosamente.`);
                 usuario = prompt('Usuario:');
             }
@@ -83,11 +89,12 @@ function addReport(user = usuario){
     report.classList.add('report');
     target.appendChild(report);
     report.appendChild(autor);
-    report.appendChild(newReport)
+    report.appendChild(newReport);
     newReport.innerHTML = prompt('Añadir reporte:');
     const fecha = new Date();
     autor.innerHTML = `${user} - ${fecha.toLocaleString()}`;
-    new Report(user, fecha, newReport.innerHTML);  
+    new Report(user, fecha, newReport.innerHTML);
+    localStorage.newReports = JSON.stringify(Report.allInstances);
     
 }
 
@@ -100,38 +107,18 @@ function readReports(report_array){
         autor.style.color = 'yellow';
         report.classList.add('report');
         newReport.innerHTML = x.txt;
-        const fecha = x.date;
-        autor.innerHTML = `${x.user} - ${fecha}`;
+        autor.innerHTML = `${x.user} - ${x.date}`;
         target.appendChild(report);
         report.appendChild(autor);
         report.appendChild(newReport);
     }
 }
 
-const consigna = `Si no llegamos, esto es el resumen:\n 
-Se necesita,\n
-Estructura HTML\n
-Variables JS necesarias (Arrays, metodos, objetos) \n
-- Metodos de busqueda (find()) y filtrado sobre el Array (filter())\n
-\n
-Proceso requerido:\n
-- Capturar entradas mediante prompt().\n
-- Declarar variables y objetos necesarios para simular el proceso seleccionado.\n
-- Crear funciones y/o métodos para realizar operaciones.\n
-- Y finalmente, efectuar una salida, que es el resultado de los datos procesados, la cual puede hacerse por alert() o console.log().
-`//porque no funciona \n ?? 
-const lorem = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non eos cum, esse quod ut dicta ea perferendis quo ab numquam! Eos consequuntur optio pariatur, provident quaerat aspernatur repudiandae quas deleniti?';
-
-const snk_report = new Report('snk',new Date(1995,3,12,13,0,0),lorem);
-const coder_report = new Report('coder', new Date(2024,4,30,20,0,0),consigna)
-const ttt_report = new Report('ttt',new Date(2024,5,1,17,30,20), 'RM 2-0 BVB');
-
-
 let usuario = prompt('Para registrarse ingrese "+newUser" \n\nUsuario:');
-//loginUser(usuario);
 usuario = loginUser(usuario);
-//snk_report.editReport(); //no funcionan los \n queda feo
 readReports(Report.allInstances);
+
+
 
 
 
